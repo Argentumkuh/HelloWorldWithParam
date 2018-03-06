a9a4ed505911811704f9a673fbecf1ea0bb730aa
Argentumkuh <32850909+Argentumkuh@users.noreply.github.com>
2018-03-06 14:05:15 +0200

    Add files via upload

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static String paramName;
+
+    public static void main(String[] args) {
+        paramName = args[0];
+
+        System.out.println(paramName);
+    }
+}
